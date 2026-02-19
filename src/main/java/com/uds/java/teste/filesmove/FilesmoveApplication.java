256963580a09488a39d8cf6fc1b752fbfad7be50
piassa157 <lindaum98@gmail.com>
2026-02-18 21:05:57 -0300

    [init-project] feat: created project with spring initlzr

---
+++ src/main/java/com/uds/java/teste/filesmove/FilesmoveApplication.java
@@ -0,0 +1,13 @@
+package com.uds.java.teste.filesmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesmoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesmoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uds/java/teste/filesmove/FilesmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uds.java.teste.filesmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesmoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
