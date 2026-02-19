f027facb1a4b3c58d80130389e7ce7589a432cdf
piassa157 <lindaum98@gmail.com>
2026-02-18 21:39:35 -0300

    [devops] feat: created application propeties and fake integration test

---
+++ src/test/java/com/uds/java/teste/filesmove/FilesmoveFakeIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.uds.java.teste.filesmove;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class FilesmoveFakeIntegrationTest {
+
+	@Test
+	void fakeIntegrationScenario() {
+		assertTrue(true);
+	}
+
+}
